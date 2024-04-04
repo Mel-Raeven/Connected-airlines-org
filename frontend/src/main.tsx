1d0f0856466224e72e7d357b91fdfc60f9a97721
Gino Stock <stockje@outlook.com>
2024-04-04 10:49:38 +0200

    Installed Mantine
    Installed Mantine as frontend framework/component provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <MantineProvider>
     <App />
-  </React.StrictMode>,
+  </MantineProvider>,
 )
