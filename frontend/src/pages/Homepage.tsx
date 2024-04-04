b85bc757a7caf90fedd0eac07544781f94f082fa
Gino Stock <stockje@outlook.com>
2024-04-04 11:07:53 +0200

    Created React router
    Created react router. Easily add new pages to it, just set a route. Please don't update the version of react-router-dome

---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "@mantine/core/styles.css";
+
+
+const Homepage: React.FC<{}> = ({}) => {
+ 
+
+  return (
+    <>
+      <h1>Homepage</h1>
+    </>
+  );
+};
+
+export default Homepage;
