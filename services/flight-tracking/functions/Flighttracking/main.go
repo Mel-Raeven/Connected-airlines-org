7995589f0efaea91196b91185416de58a882dbdf
MennoVanCranenbroek <mennovc2001@gmail.com>
2024-06-12 13:36:51 +0200

    flight generation now loop of 10

--- services/flight-tracking/functions/Flighttracking/main.go
+++ services/flight-tracking/functions/Flighttracking/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
-	"os"
 	"strconv"
 	"time"
 
@@ -16,44 +15,46 @@ import (
 )
 
 func GenerateflightData(ctx context.Context) error {
-	cfg, err := config.LoadDefaultConfig(ctx)
-	if err != nil {
-		return err
-	}
+    cfg, err := config.LoadDefaultConfig(ctx)
+    if err != nil {
+        return err
+    }
 
-	client := dynamodb.NewFromConfig(cfg)
+    client := dynamodb.NewFromConfig(cfg)
 
-	fmt.Println("generating random flight data")
-	// Seed the random number generator to produce different results each time
-	rand.Seed(time.Now().UnixNano())
-	min := -90.0
-	max := 90.0
+    fmt.Println("generating random flight data")
+    // Seed the random number generator to produce different results each time
+    rand.Seed(time.Now().UnixNano())
+    min := -90.0
+    max := 90.0
 
-	randomlong := min + rand.Float64()*(max-min)
-	randomlat := min + rand.Float64()*(max-min)
-	randomdegree := rand.Intn(360)
+    for i := 1; i <= 10; i++ {
+        randomlong := min + rand.Float64()*(max-min)
+        randomlat := min + rand.Float64()*(max-min)
+        randomdegree := rand.Intn(360)
 
-	item := map[string]types.AttributeValue{
-		"FlightId":  &types.AttributeValueMemberS{Value: "1"},
-		"Longitude": &types.AttributeValueMemberN{Value: fmt.Sprintf("%f", randomlong)},
-		"Lattitude": &types.AttributeValueMemberN{Value: fmt.Sprintf("%f", randomlat)},
-		"Degree":    &types.AttributeValueMemberN{Value: strconv.Itoa(randomdegree)},
-	}
+        item := map[string]types.AttributeValue{
+            "FlightId":  &types.AttributeValueMemberS{Value: strconv.Itoa(i)},
+            "Longitude": &types.AttributeValueMemberN{Value: fmt.Sprintf("%f", randomlong)},
+            "Lattitude": &types.AttributeValueMemberN{Value: fmt.Sprintf("%f", randomlat)},
+            "Degree":    &types.AttributeValueMemberN{Value: strconv.Itoa(randomdegree)},
+        }
 
-	// Define input parameters for PutItem operation
-	input := &dynamodb.PutItemInput{
-		TableName: aws.String("FlightTrackingDynamoDBTable"),
-		Item:      item,
-	}
+        // Define input parameters for PutItem operation
+        input := &dynamodb.PutItemInput{
+            TableName: aws.String("FlightTrackingDynamoDBTable"),
+            Item:      item,
+        }
 
-	// Perform PutItem operation
-	fmt.Println("inserting flight data")
-	_, err = client.PutItem(context.Background(), input)
-	if err != nil {
-		fmt.Println("Error putting item to DynamoDB:", err)
-		os.Exit(1)
-	}
-	return nil
+        // Perform PutItem operation
+        fmt.Println("inserting flight data for FlightId", i)
+        _, err = client.PutItem(context.Background(), input)
+        if err != nil {
+            fmt.Println("Error putting item to DynamoDB:", err)
+            return err
+        }
+    }
+    return nil
 }
 
 func HandleRequest(ctx context.Context, event *MyEvent) error {
